6c999911d01b0d99ec470158f05c3d4fe43b3fc8
Vishnunarayan K I <appukuttancr@gmail.com>
2018-09-07 19:36:25 +0530

    lazyload commands

--- codechef_cli/cli.py
+++ codechef_cli/cli.py
@@ -10,11 +10,25 @@ from codechef_cli.__version__ import __version__
 echo = click.echo
 
 
-@click.group(context_settings=Config.CONTEXT_SETTINGS)
-@click.version_option(version=__version__)
-def cli():
-    """Codechef CLI
+plugin_folder = os.path.join(os.path.dirname(__file__), 'commands')
 
-    Use codechef from your terminal
-    """
-    pass
+class CLIClass(click.MultiCommand):
+
+    def list_commands(self, ctx):
+        rv = []
+        for filename in os.listdir(plugin_folder):
+            if filename.endswith('.py'):
+                rv.append(filename[:-3])
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        ns = {}
+        fn = os.path.join(plugin_folder, name + '.py')
+        with open(fn) as f:
+            code = compile(f.read(), fn, 'exec')
+            eval(code, ns, ns)
+        return ns['cli']
+
+
+cli = CLIClass(context_settings=Config.CONTEXT_SETTINGS, help="Codechef cli")
